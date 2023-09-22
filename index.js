cd5db3d0db3d31ee14d30e60b6a84e4b8131d156
Dekna <kdkrisnaaa@gmail.com>
2023-09-22 17:18:06 +0800

    Added styling at Form

---
+++ index.js
@@ -0,0 +1,7 @@
+function handleGetFormData(){
+    let name = document.getElementById("name").value;
+    let city = document.getElementById("city").value;
+    let email = document.getElementById("email").value;
+    let zipCode = document.getElementById("zip-code").value;
+    let status = document.getElementById("status").value;
+}
